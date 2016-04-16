a2aa216dc107cba7e7424a43a84a268c411297be
Joel <joel.krebs@gmail.com>
2016-04-16 16:38:18 -0700

    add irc ads to ad panda

--- wp-content/plugins/cets-ad-panda/ads.php
+++ wp-content/plugins/cets-ad-panda/ads.php
@@ -1,25 +1,29 @@
 <?php
 
-
-
-function cets_ad_panda_get_ads() {
-
-$ads = array();
-
-$ads[] = array(
-	'id' => 'default',
-	'title' => 'Default Magic Judges',
-);
-
-$ads[] = array(
-	'id' => 'jace',
-	'title' => 'Jace - We Want You',
-);
-
-$ads[] = array(
-	'id' => 'blogportal',
-	'title' => 'Blog Portal - Domens Gate Graphic',
-);
+function cets_ad_panda_get_ads()
+{
+	$ads = array(
+		array(
+			'id' => 'default',
+			'title' => 'Default Magic Judges',
+		),
+		array(
+			'id' => 'jace',
+			'title' => 'Jace - We Want You',
+		),
+		array(
+			'id' => 'blogportal',
+			'title' => 'Blog Portal - Domens Gate Graphic',
+		),
+		array(
+			'id' => 'mtgjudge',
+			'title' => 'MTG Judge IRC Chat',
+		),
+		array(
+			'id' => 'mtgrules',
+			'title' => 'MTG Rules IRC Chat',
+		),
+	);
 
 	return $ads;
 }
---
+++ wp-content/plugins/cets-ad-panda/ads/mtgjudge/ad.php
@@ -0,0 +1,6 @@
+<?php
+$img_url = plugins_url('mtgjudge.jpg', __FILE__);
+?>
+<a href="http://chat.magicjudges.org/mtgjudge/">
+  <img src="<?php echo $img_url; ?>" style="max-width: 100%;" />
+</a>
