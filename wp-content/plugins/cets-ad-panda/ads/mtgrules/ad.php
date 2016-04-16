5029b66f2b6bb2b7c5aa51e02ebe9b40b41d5603
Joel <joel.krebs@gmail.com>
2016-04-16 16:38:18 -0700

    add irc ads to ad panda

---
+++ wp-content/plugins/cets-ad-panda/ads/mtgrules/ad.php
@@ -0,0 +1,6 @@
+<?php
+$img_url = plugins_url('mtgrules.jpg', __FILE__);
+?>
+<a href="http://chat.magicjudges.org/mtgrules/">
+  <img src="<?php echo $img_url; ?>" style="max-width: 100%;" />
+</a>
