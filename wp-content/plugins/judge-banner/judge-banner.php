9fad5e81a2cf309434b7a64f01fab512962af24d
Steffen <steffen@baumgart.biz>
2017-03-06 19:12:56 +0100

    updated judge-banner URL

--- wp-content/plugins/judge-banner/judge-banner.php
+++ wp-content/plugins/judge-banner/judge-banner.php
@@ -12,37 +12,13 @@ License: GPL2
 add_action("wp_enqueue_scripts", "judge_banner_scripts");
 
 function judge_banner_scripts() {
-	/*if (wp_get_theme()->get_stylesheet() === "judge-familiar") {
-		wp_enqueue_style(
-			"judge-banner-screen",
-			"http://assets.magicjudges.org/judge-banner/css/screen.css",
-			array(),
-			"1.0",
-			"(min-width: 1001px)"
-		);
-		wp_enqueue_style(
-			"judge-banner-mobile",
-			"http://assets.magicjudges.org/judge-banner/css/mobile.css",
-			array(),
-			"1.0",
-			"(max-width: 1000px)"
-		);
-		wp_enqueue_script(
-			"judge-banner-script",
-			"http://assets.magicjudges.org/judge-banner/js/judge-banner.js",
-			array(),
-			"1.0",
-			true // $in_footer
-		);
-	} else {*/
+
 		// Fallback for the world before judge-familiar
 		wp_enqueue_script(
 			"judge-banner",
-			"http://assets.magicjudges.org/judge-banner/judge-banner.js",
-			array("jquery"),
-			"0.4"
+			"https://assets.magicjudges.org/judge-banner/judge-banner.js",
+			array("jquery")
 		);
-	//}
 }
 
 function judge_banner() {
